072e39792e4f48024f77c40ff3c019243464a0c6
SaidaloFront77 <saydazimovsaidaloo@gmail.com>
2025-06-19 23:17:37 +0500

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fullstack-backend-toyxona.onrender.com',
 });
 
 instance.interceptors.request.use(config => {
